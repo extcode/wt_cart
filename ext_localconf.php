e0aa86dd8733d69d0dc1ac65f9dc2f13d4293623
Daniel Lorenz <daniel.lorenz@extco.de>
2013-03-25 23:18:18 +0100

    Issue #41587: changes pi3 from USER to USER_INT to prevent caching of minicart

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@ if (!defined ('TYPO3_MODE')) {
 
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_wtcart_pi1.php', '_pi1', 'list_type', 0);
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi2/class.tx_wtcart_pi2.php', '_pi2', 'list_type', 1);
-t3lib_extMgm::addPItoST43($_EXTKEY, 'pi3/class.tx_wtcart_pi3.php', '_pi3', 'list_type', 2);
+t3lib_extMgm::addPItoST43($_EXTKEY, 'pi3/class.tx_wtcart_pi3.php', '_pi3', 'list_type', 0);
 
 # Hook: clear powermail output if session is not filled
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['powermail']['PM_MainContentHookAfter'][]	= 'EXT:wt_cart/lib/class.tx_wtcart_powermail.php:tx_wtcart_powermail';
